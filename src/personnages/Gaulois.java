ed1c3f07e22f1aaedecdfd36938962670b0ed137
Sarah Laouisset <lss4789a@ensfsi.onmicrosoft.com>
2024-09-18 15:33:25 +0200

    fin tp

---
+++ src/personnages/Gaulois.java
@@ -0,0 +1,51 @@
+package personnages;
+
+public class Gaulois {
+	private String nom;
+	private int force;
+	private int effetPotion = 1;
+
+	public Gaulois(String nom, int force, int effetPotion) {
+		this.nom = nom;
+		this.force = force;
+		this.effetPotion = effetPotion;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public int getForce() {
+		return force;
+	}
+
+	public int getEffetPotion() {
+		return effetPotion;
+	}
+
+	public void parler(String texte) {
+		System.out.println(prendreParole() + "« " + texte + "»");
+	}
+
+	private String prendreParole() {
+		return "Le gaulois " + nom + " : ";
+	}
+
+//	public void frapper(Romain romain) {
+//		System.out.println(nom + " envoie un grand coup dans la mâchoire de " + romain.getNom());
+//		romain.recevoirCoup(force / 3);
+//
+//	}
+
+	public void setEffetPotion(int effetPotion) {
+		this.effetPotion = effetPotion;
+	}
+
+	@Override
+	public String toString() {
+		return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]";
+	}
+public static void main(String[] args) {
+	
+}
+}
---
+++ src/personnages/Romain.java
@@ -0,0 +1,36 @@
+package personnages;
+
+public class Romain {
+	private int force;
+	private String nom;
+
+	public Romain(String nom, int force) {
+		this.nom = nom;
+		this.force = force;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void parler(String texte) {
+		System.out.println(prendreParole() + "« " + texte + "»");
+	}
+
+	private String prendreParole() {
+		return "Le romain " + nom + " : ";
+	}
+
+	public void recevoirCoup(int forceCoup) {
+		force -= forceCoup;
+		if (force > 0) {
+			parler("Aïe");
+		} else {
+			parler("J'abandonne...");
+		}
+	}
+public static void main(String[] args) {
+	
+}
+}
+
